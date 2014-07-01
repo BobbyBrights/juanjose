f46363f30104779b93e262fb94c08747e95132e8
Jose Ignacio Navarro <josencv@gmail.com>
2014-07-01 13:06:14 -0400

    joomla installed

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-06-30 21:27:17 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-06-30T21:27:17+00:00	INFO	cookiefailure	Username and password do not match or you do not have an account yet.
